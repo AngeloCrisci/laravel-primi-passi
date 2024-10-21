4fd211f2743fe08a234ab6bea57f41189ab377f9
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-21 17:21:27 +0200

    Trying the bonus

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>home</title>
+    <title>News</title>
   </head>
   <body>
     <main>
@@ -13,7 +13,7 @@
         <header>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container">
-                  <a class="navbar-brand" href="http://127.0.0.1:8000/">HOME</a>
+                  <a class="navbar-brand" href="#">HOME</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/' , function () {
+        $contacts = [
+            'contatto 1',
+            'contatto 2',
+            'contatto 3',
+        ];
+        $sponsor = 'SONO UNA PAGINA SPONSOR';
+        $news = 'SONO TANTI ARTICOLI';
+
+        return view('admin.pages.index', compact("contacts ", "sponsor" , "news" ));
+});
+
 Route::get('/', function () {
     $title = 'Hello World';
 
